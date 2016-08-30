87d35675235c7732897fc4d7d52f8a012bcef9df
luckymore <njuluckwang@gmail.com>
2016-08-30 20:00:00 +0800

    update the exam docs

--- doc/controllers/Exam.js
+++ doc/controllers/Exam.js
@@ -6,6 +6,18 @@ var url = require('url');
 var Exam = require('./ExamService');
 
 
+module.exports.examsExam_idDELETE = function examsExam_idDELETE (req, res, next) {
+  Exam.examsExam_idDELETE(req.swagger.params, res, next);
+};
+
+module.exports.examsExam_idGET = function examsExam_idGET (req, res, next) {
+  Exam.examsExam_idGET(req.swagger.params, res, next);
+};
+
+module.exports.examsGET = function examsGET (req, res, next) {
+  Exam.examsGET(req.swagger.params, res, next);
+};
+
 module.exports.examsPOST = function examsPOST (req, res, next) {
   Exam.examsPOST(req.swagger.params, res, next);
 };
--- doc/controllers/ExamService.js
+++ doc/controllers/ExamService.js
@@ -1,9 +1,73 @@
 'use strict';
 
+exports.examsExam_idDELETE = function(args, res, next) {
+  /**
+   * parameters expected in the args:
+  **/
+    var examples = {};
+  examples['application/json'] = {
+  "result" : 123,
+  "message" : "aeiou"
+};
+  if(Object.keys(examples).length > 0) {
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify(examples[Object.keys(examples)[0]] || {}, null, 2));
+  }
+  else {
+    res.end();
+  }
+  
+}
+
+exports.examsExam_idGET = function(args, res, next) {
+  /**
+   * parameters expected in the args:
+  **/
+    var examples = {};
+  examples['application/json'] = {
+  "group_id" : "aeiou",
+  "project_id" : "aeiou",
+  "name" : "aeiou",
+  "description" : "aeiou",
+  "id" : "aeiou"
+};
+  if(Object.keys(examples).length > 0) {
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify(examples[Object.keys(examples)[0]] || {}, null, 2));
+  }
+  else {
+    res.end();
+  }
+  
+}
+
+exports.examsGET = function(args, res, next) {
+  /**
+   * parameters expected in the args:
+  * group_id (String)
+  **/
+    var examples = {};
+  examples['application/json'] = [ {
+  "group_id" : "aeiou",
+  "project_id" : "aeiou",
+  "name" : "aeiou",
+  "description" : "aeiou",
+  "id" : "aeiou"
+} ];
+  if(Object.keys(examples).length > 0) {
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify(examples[Object.keys(examples)[0]] || {}, null, 2));
+  }
+  else {
+    res.end();
+  }
+  
+}
+
 exports.examsPOST = function(args, res, next) {
   /**
    * parameters expected in the args:
-  * exam (Exam)
+  * name (Name)
   **/
     var examples = {};
   examples['application/json'] = {
