8b2c0e8129934242b3c7afd832e680c40129ff72
AliServer <njuluckwang@gmail.com>
2016-09-19 14:57:30 +0800

    commit by server

--- app/config.py
+++ app/config.py
@@ -1,7 +1,7 @@
 # cheHost = "http://115.29.184.56:8080/api/"
 dbHost = "localhost:3306"
-dbUser = "root"
-dbPass = "root"
+dbUser = "debian-sys-maint"
+dbPass = "pyqhvhE5AEKJ2gzX"
 dbName = "che"
 
 DB_URL = "mysql://" + dbUser + ":" + dbPass + "@" + dbHost + "/" + dbName
